657cd663b4d2cfec88002785cf273d52feb1d800
Muhammad Rifqi Amir Putra <muhammadrifqiamirp20@gmail.com>
2025-07-07 01:04:59 +0700

    feat: add NotFound component to handle 404 errors and redirect users

--- next.config.mjs
+++ next.config.mjs
@@ -9,19 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/error-404/:path*',
-        destination: '/500', // Arahkan ke halaman kosong (bukan /404)
-      },
-      {
-        source: '/error-pages/:path*',
-        destination: '/500', // Blokir akses ke halaman 401/503
-      }
-    ]
-  },
-  generateBuildId: async () => 'build-' + Date.now(),
 }
 
 export default nextConfig
